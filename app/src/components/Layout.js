b927ed8386aa7e4888b859fd36394926c368619b
Jordan Ivy <jordan.mixon@bobcats.gcsu.edu>
2019-07-16 22:51:40 -0400

    - member directory basic layout
    - add material-ui
    - add member directory basic layout
    - add webdevatlanta logo
    - update/rearrange template code
    - update package.json as needed

---
+++ app/src/components/Layout.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import BottomAppBar from './BottomAppBar';
+import WebDevAtlantaLogo from '../assets/images/WDA-logo.png'
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export default function Layout() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Grid container spacing={2} justify="center">
+              <Grid item>
+                <img id="logo" src={WebDevAtlantaLogo} alt="WebDevAtlanta primary logo" />
+              </Grid>
+              <Grid item>
+                <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                  Web Dev Atlanta is a meetup focused on web and software development. We meet and work on group projects and discuss topics in the area of web development. This group is open to all levels of coders!
+            </Typography>
+              </Grid>
+            </Grid>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Join Us!
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image=""
+                    title="Member"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      This could be you!
+                    </Typography>
+                    <Typography>
+                      Join WebDevAtlanta today.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+        <BottomAppBar></BottomAppBar>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          &copy; WebDevAtlanta 2019
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          -social media here-
+        </Typography>
+      </footer>
+      {/* End footer */}
+    </React.Fragment >
+  );
+}
