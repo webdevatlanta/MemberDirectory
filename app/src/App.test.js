ccfc6671b459c034a7210c225107149c0512c7ad
abrie <webdevatlanta@abrie.fastmail.com>
2019-07-22 15:43:22 -0400

    Fix the "renders without crashing" unit test. Wrap update methods in act() calls, and manually resolve the fetch.

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -4,7 +4,17 @@ import App from './App';
 import { act } from 'react-dom/test-utils';
 
 it('renders without crashing', () => {
+  fetch.resetMocks();
+
+  let doResolve;
+  fetch.mockResponse(() => {
+    return new Promise( (resolve) => {
+      doResolve = resolve;
+    })
+  });
+
   const div = document.createElement('div');
   act( () => { ReactDOM.render(<App />, div) } );
+  act( () => { doResolve("gist contents here, same for all.") });
   ReactDOM.unmountComponentAtNode(div);
 });
