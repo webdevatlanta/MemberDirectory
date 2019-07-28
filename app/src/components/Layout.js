ec5e60420d04c9f2632c13015bd137d4dc60861c
Christopher Adams <chrismichaeladams@gmail.com>
2019-07-27 21:02:39 -0400

    Make cards draggable/movable on screen

--- app/src/components/Layout.js
+++ app/src/components/Layout.js
@@ -10,7 +10,8 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import BottomAppBar from './BottomAppBar';
-import WebDevAtlantaLogo from '../assets/images/WDA-logo.png'
+import WebDevAtlantaLogo from '../assets/images/WDA-logo.png';
+import Draggable, {DraggableCore} from 'react-draggable';
 
 const useStyles = makeStyles(theme => ({
   icon: {
@@ -84,31 +85,41 @@ export default function Layout(props) {
           <Grid container spacing={4}>
             {props.data.cards.filter(card => !card.error).map(card => (
               <Grid item key={card.name} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image={card.profile.avatar}
-                    title="Member"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {card.name}
-                    </Typography>
-                    <Typography>
-                      {card.profile.status}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <a href={ card.github_url }>
-                      <Button className={ classes.palette } size="small" color="primary" >
-                        View
-                      </Button>
-                    </a>
-                  </CardActions>
-                </Card>
+                <Draggable
+                  axis="both"
+                  handle=".handle"
+                  defaultPosition={{x: 0, y: 0}}
+                  position={null}
+                  grid={[25, 25]}
+                  scale={1}
+                >
+                  <Card className={[classes.card, "handle"]} style={{cursor: 'grab'}}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image={card.profile.avatar}
+                      title="Member"
+                    />
+                    <CardContent className={classes.cardContent}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {card.name}
+                      </Typography>
+                      <Typography>
+                        {card.profile.status}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <a href={ card.github_url }>
+                        <Button className={ classes.palette } size="small" color="primary" >
+                          View
+                        </Button>
+                      </a>
+                    </CardActions>
+                  </Card>
+                </Draggable>
               </Grid>
             ))}
           </Grid>
+          
 
           {/* Cards with errors are shown here */}
           <Grid container spacing={4}>
