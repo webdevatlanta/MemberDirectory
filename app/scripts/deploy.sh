115ed0da84244e9c264866bd1f92635092285722
abrie <abrie@users.noreply.github.com>
2019-08-05 23:09:54 -0400

    Implemented script to automate gh-pages deployment. Invoke the script with 'npm run deploy'.

---
+++ app/scripts/deploy.sh
@@ -0,0 +1,17 @@
+# If an error occurs, exit.
+trap 'exit' ERR
+
+# Connect the origin/gh-pages branch to a folder of the same name.
+git worktree add gh-pages origin/gh-pages
+
+# Remove everything in there.
+(cd gh-pages; git rm -r *)
+
+# Now move everything back (including new stuff, if available).
+cp -R build/ gh-pages/
+
+# And now commit and push the contents.
+(cd gh-pages; git add .; git commit -m "Deploy."; git push origin HEAD:gh-pages)
+
+# Then clean up (unmount) the gh-pages worktree.
+git worktree remove gh-pages
