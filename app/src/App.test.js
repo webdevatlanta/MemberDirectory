b2446f54e489e29781f92ebb9b7f58d75cf3b4d6
abrie <abrie@users.noreply.github.com>
2019-08-12 16:06:43 -0400

    Use new React 16.9 async act().

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -4,37 +4,17 @@ import App from './App';
 import {act} from 'react-dom/test-utils';
 import Config from './config.test.json';
 
-it('renders without crashing', () => {
+it('renders without crashing', async () => {
   fetch.resetMocks();
 
-  /* Very hacky solution here; using a timeout to ensure all
-   * the fetch mocks complete before the component unmounts. */
-
-  let resolveMemberList;
-  fetch.mockResponses(
-    [
-      () =>
-        new Promise(resolve => {
-          resolveMemberList = resolve;
-        }),
-    ],
-    [TEST_FOO_PROFILE],
-    [
-      () =>
-        new Promise(resolve =>
-          setTimeout(() => resolve({body: TEST_BAR_PROFILE}), 0),
-        ),
-    ],
-  );
+  fetch.mockResponseOnce( JSON.stringify(TEST_MEMBER_MASTERLIST) );
+  fetch.mockResponseOnce( JSON.stringify(TEST_FOO_PROFILE) );
+  fetch.mockResponseOnce( JSON.stringify(TEST_BAR_PROFILE) );
 
   const div = document.createElement('div');
 
-  act(() => {
+  await act( async () => {
     ReactDOM.render(<App config={Config} />, div);
-  });
-
-  act(() => {
-    resolveMemberList({body: JSON.stringify(TEST_MEMBER_MASTERLIST)});
   });
 
   ReactDOM.unmountComponentAtNode(div);
