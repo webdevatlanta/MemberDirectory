2f395403e8f30ffa17c2a94d10361a100dd2d726
abrie <abrie@users.noreply.github.com>
2019-10-02 09:16:19 -0400

    Refactor 'Hero' content into component.

--- app/src/components/Layout.js
+++ app/src/components/Layout.js
@@ -1,28 +1,14 @@
 import React, {useState} from 'react';
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import {makeStyles} from '@material-ui/styles';
-import Container from '@material-ui/core/Container';
 import TopAppBar from './TopAppBar';
-import WebDevAtlantaLogo from '../assets/images/WDA-logo.png';
-import CardGrid from './CardGrid';
 import CardGame from './CardGame';
 import AdminPanel from './AdminPanel';
+import HeroPanel from './HeroPanel';
 
 function themedStyles(theme) {
   return {
-    icon: {
-      marginRight: theme.spacing(2),
-    },
-    heroContent: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(8, 0, 6),
-    },
-    heroButtons: {
-      marginTop: theme.spacing(4),
-    },
     footer: {
       backgroundColor: theme.palette.background.paper,
       padding: theme.spacing(6),
@@ -50,51 +36,10 @@ export default function({cards, config}) {
         }
 
         {
-          (!mode.admin && !mode.game) && <>
-        {/* Hero unit */}
-        <div className={classes.heroContent}>
-          <Container maxWidth="sm">
-            <Grid container spacing={2} justify="center">
-              <Grid item>
-                <img
-                  id="logo"
-                  src={WebDevAtlantaLogo}
-                  alt="WebDevAtlanta primary logo"
-                />
-              </Grid>
-              <Grid item>
-                <Typography
-                  variant="h5"
-                  align="center"
-                  color="textSecondary"
-                  paragraph>
-                  Web Dev Atlanta is a meetup focused on web and software
-                  development. We meet and work on group projects and discuss
-                  topics in the area of web development. This group is open to
-                  all levels of coders!
-                </Typography>
-              </Grid>
-            </Grid>
-            <div className={classes.heroButtons}>
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button
-                    variant="contained"
-                    color="secondary"
-                    href="https://www.meetup.com/webdevatlanta/"
-                    target="_window">
-                    Join Us!
-                  </Button>
-                </Grid>
-              </Grid>
-            </div>
-          </Container>
-        </div>{' '} {/* End hero unit */}
-        <CardGrid cards={cards}></CardGrid>
-        </>
-      }
+          (!mode.admin && !mode.game) && <HeroPanel cards={cards}></HeroPanel>
+        }
       </main>
-      {/* Footer */}
+
       <footer className={classes.footer}>
         <Typography variant="h6" align="center" gutterBottom>
           &copy; WebDevAtlanta 2019
@@ -107,7 +52,7 @@ export default function({cards, config}) {
           -social media here-
         </Typography>
       </footer>
-      {/* End footer */}
+
     </>
   );
 }
